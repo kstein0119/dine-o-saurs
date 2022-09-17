c33471dbe0c2cd2372057ee97da1d80a4bea4109
Kayla Stein <kstein0119@gmail.com>
2022-09-17 17:51:40 -0400

    Added walking animations
    Fixed blinking issue and added walking animations

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    //public Animator animator;
+    public Animator animator;
     // Set default values for moveSpeed and jumpHeight, but also editable in Unity UI
 
     void Update()
     {
 
-        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position = transform.position + horizontal * Time.deltaTime * moveSpeed;
