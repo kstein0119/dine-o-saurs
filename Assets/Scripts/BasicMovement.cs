017a76658a527f67a9bb77a2bb01cda16b2df169
Kayla Stein <kstein0119@gmail.com>
2022-09-17 16:52:58 -0400

    Basic movement
    Added movement mechanic and other frames for the walking animation. adding those soon

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    //public Animator animator;
+    // Set default values for moveSpeed and jumpHeight, but also editable in Unity UI
+
+    void Update()
+    {
+
+        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position = transform.position + horizontal * Time.deltaTime * moveSpeed;
+
+    }
+}
